8e88cd78336854a9bb3d791eee232cceb946fe8a
!(geek) <93247093+Sadhana1842@users.noreply.github.com>
2024-07-26 18:15:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.25%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    void solve(vector<int> &nums,vector<int> &check,vector<vector<int>> &ans_fin,int n,int ind){
-        if(ind==n){
-            vector<int>ans;
-            for(int i=0;i<n;i++){
-                if(check[i]==1){
-                    ans.push_back(nums[i]);
-
-                }
-
-            }
-            ans_fin.push_back(ans);
-            return;
-        }
-        else{
-            check[ind]=1;
-            solve(nums,check,ans_fin,n,ind+1);
-            check[ind]=-1;
-            solve(nums,check,ans_fin,n,ind+1);
-        }
-
-
-
+    void solve(vector<int> &nums, vector<int> &current, vector<vector<int>> &ans_fin, int n, int ind) {
+    if (ind == n) {
+        ans_fin.push_back(current);
+        return;
+    } else {
+        // Include the current element
+        current.push_back(nums[ind]);
+        solve(nums, current, ans_fin, n, ind + 1);
+        current.pop_back();  // Backtrack
+        
+        // Exclude the current element
+        solve(nums, current, ans_fin, n, ind + 1);
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>check(n,-1);
-        vector<int>ans;
-        vector<vector<int>>ans_fin;
-        solve(nums,check,ans_fin,n,0);
-        return ans_fin;
-    }
+}
+
+vector<vector<int>> subsets(vector<int> &nums) {
+    vector<vector<int>> ans_fin;
+    vector<int> current;
+    solve(nums, current, ans_fin, nums.size(), 0);
+    return ans_fin;
+}
+
 };

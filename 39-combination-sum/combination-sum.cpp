6957c9294d56c8dcbb897c6f3db4a6a6c0555dc7
!(geek) <93247093+Sadhana1842@users.noreply.github.com>
2024-07-26 18:33:14 +0530

    Time: 4 ms (72.05%) | Memory: 12.4 MB (61.88%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void findcom(int ind,int target,vector<int>& arr,vector<vector<int>>&ans,vector<int>&ds){
+        if(ind==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            findcom(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+        }
+        findcom(ind+1,target,arr,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findcom(0,target,candidates,ans,ds);
+        return ans;
+    }
+    
+};
